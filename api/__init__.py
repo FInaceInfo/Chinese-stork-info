3d62c7ab8e2bede7bbc4961f306f18cf9d8b6cc8
HUANG SIZHE <hsz1273327@gmail.com>
2017-02-26 00:22:50 +0800

    logger
    logger

--- api/__init__.py
+++ api/__init__.py
@@ -4,6 +4,13 @@ from .stock_basic import *
 from .stock_report import *
 from .stock_index import *
 app = Flask(__name__)
+if not app.debug:
+    import logging
+    from logging.handlers import RotatingFileHandler
+    file_handler = RotatingFileHandler('box-office.log', maxBytes=10*1024*1024,backupCount=5)
+    file_handler.setLevel(logging.WARNING)
+    app.logger.addHandler(file_handler)
+
 api = Api(app)
 
 @api.representation('application/json')
--- app/__init__.py
+++ app/__init__.py
@@ -5,6 +5,13 @@ from .stock_report import *
 from .stock_index import *
 
 app = Flask(__name__)
+if not app.debug:
+    import logging
+    from logging.handlers import RotatingFileHandler
+    file_handler = RotatingFileHandler('box-office.log', maxBytes=10*1024*1024,backupCount=5)
+    file_handler.setLevel(logging.WARNING)
+    app.logger.addHandler(file_handler)
+    
 socketio = SocketIO(app)
 
 
